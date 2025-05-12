c632e3069a5f99ebe2c667d6344f9d58376dab5f
aliem99 <aliemwidjaja@gmail.com>
2025-05-12 19:18:06 +0700

    revisi

--- chats/models.py
+++ chats/models.py
@@ -9,11 +9,7 @@ CHAT_ROLE_CHOICES = (
     (CHAT_ROLE_ASSISTANT, "Assistant"),
 )
 
-class ChatRoom(BaseModel):
-    name = models.CharField(max_length=255)
-
 class Chat(BaseModel):
     message = models.TextField()
     role = models.CharField(max_length=50, choices=CHAT_ROLE_CHOICES, default=CHAT_ROLE_USER)
     contract = models.ForeignKey("documents.Contract", on_delete=models.SET_NULL, null=True)
-    chatroom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name="chats")
