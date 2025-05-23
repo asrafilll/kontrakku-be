d8b4d22fc8a657657152cf004a0ec8409297d523
lolikgiovi <lolik.giovi@gmail.com>
2025-05-23 23:10:34 +0700

    fix

--- core/asgi.py
+++ core/asgi.py
@@ -2,11 +2,21 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application  # optional fallback
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-from .urls import websocket_urlpatterns
+# Don't import routing at top-level!
+django_application = get_asgi_application()
+
+# Import AFTER Django is ready
+from core.urls import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
-    {"http": get_asgi_application(), "websocket": URLRouter(websocket_urlpatterns)}
+    {
+        "http": django_application,
+        "websocket": URLRouter(websocket_urlpatterns),
+    }
 )
+
+## Yak betul fixingan dibantu chatgpt but it works yey
